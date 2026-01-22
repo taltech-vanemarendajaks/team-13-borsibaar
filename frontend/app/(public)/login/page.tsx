ea8c7243676ef5d8350a5d1425f3b5162f59a408
Kairo Kõrgend <kairo@excellent.ee>
2026-01-22 13:14:46 +0200

    Remove unnecessary type=button attribute

--- frontend/app/(public)/login/page.tsx
+++ frontend/app/(public)/login/page.tsx
@@ -9,10 +9,7 @@ export default function LoginPage() {
     <div className="flex flex-col min-h-screen items-center justify-center gap-4">
       <h1 className="text-2xl font-bold">Login</h1>
       <a href={`${publicBackendUrl}/oauth2/authorization/google`}>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
+        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
           Login with Google
         </button>
       </a>
